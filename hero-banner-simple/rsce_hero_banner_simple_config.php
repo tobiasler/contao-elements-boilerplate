8b8a1b37eeb6bb274445c9a4f2eeac96816af193
tobiasler <basler.tobias02@gmail.com>
2025-02-12 18:49:58 +0100

    update hero-banner-simple
    remove generate method from config, not working with RSCE

--- hero-banner-simple/rsce_hero_banner_simple_config.php
+++ hero-banner-simple/rsce_hero_banner_simple_config.php
@@ -26,28 +26,4 @@ return array(
             ),
         ),
     ),
-    'generate' => function(array $attributes) {
-        // UUID zu Pfad und Alt-Text auflösen
-        $backgroundImagePath = '';
-        $backgroundImageAlt = '';
-        
-        if (!empty($attributes['backgroundImage'])) {
-            $fileModel = \Contao\FilesModel::findByUuid($attributes['backgroundImage']);
-            if ($fileModel !== null) {
-                $backgroundImagePath = $fileModel->path;
-
-                // Alt-Text aus den Metadaten des Bildes abrufen
-                $meta = $fileModel->getMeta();
-                if (!empty($meta['alt'])) {
-                    $backgroundImageAlt = $meta['alt'];
-                }
-            }
-        }
-
-        // Rückgabe der erweiterten Daten an das Template
-        return array_merge($attributes, array(
-            'backgroundImagePath' => $backgroundImagePath,
-            'backgroundImageAlt'  => $backgroundImageAlt,
-        ));
-    },
 );
